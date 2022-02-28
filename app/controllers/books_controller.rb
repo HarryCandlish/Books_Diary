66568a2a08d51fe96e7aa5831cca742ca23f7afa
Harry Candlish <hcnd90@gmail.com>
2022-02-28 16:05:46 +1300

    user authentication

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,9 @@
 class BooksController < ApplicationController
   before_action :set_book, only: %i[ show edit update destroy ]
 
+  before_action :authenticate_user!, except: [:index]
+
+  before_action :correct_user, only: [:show, :edit, :update, :destroy]
   # GET /books or /books.json
   def index
     @books = Book.all
@@ -12,7 +15,8 @@ class BooksController < ApplicationController
 
   # GET /books/new
   def new
-    @book = Book.new
+    # @book = Book.new
+    @book = current_user.books.build
   end
 
   # GET /books/1/edit
@@ -21,7 +25,8 @@ class BooksController < ApplicationController
 
   # POST /books or /books.json
   def create
-    @book = Book.new(book_params)
+    # @book = Book.new(book_params)
+    @book = current_user.books.build(book_params)
 
     respond_to do |format|
       if @book.save
@@ -57,6 +62,11 @@ class BooksController < ApplicationController
     end
   end
 
+    def correct_user
+      @book = current_user.books.find_by(id: params[:id])
+      redirect_to books_path, notice: "Not Authorised" if @book.nil?
+    end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_book
@@ -65,6 +75,6 @@ class BooksController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def book_params
-      params.require(:book).permit(:title, :author, :description, :read, :pages, :image)
+      params.require(:book).permit(:title, :author, :description, :read, :pages, :image, :user_id)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_02_25_200533) do
+ActiveRecord::Schema[7.0].define(version: 2022_02_26_223603) do
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.string "author"
@@ -20,6 +20,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_02_25_200533) do
     t.string "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_books_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
